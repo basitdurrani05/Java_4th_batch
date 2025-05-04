d271d51b2fc77d943b465e16a0406d87c388db12
basitdurrani <basitdurrani05786@gmail.com>
2025-05-04 19:50:30 +0530

    Updated

---
+++ Object Oriented Programming/first_oops.java
@@ -0,0 +1,33 @@
+// Class and Object
+
+public class first_oops {
+
+
+    public void first(){
+
+        System.out.println("Mohan");
+    }
+
+    public void second(){
+
+        System.out.println("Basit");
+    }
+
+
+    public void third(){
+
+        System.out.println("Daivik");
+    }
+
+
+
+    public static void main(String[] args) {
+
+        first_oops sc = new first_oops();
+
+        sc.first();
+        sc.second();
+        sc.third();
+        
+    }
+}
---
+++ twelfth.java
@@ -0,0 +1,26 @@
+// Recursion - Function / Method call itself
+
+public class twelfth {
+
+
+    // public static void first(){
+
+    //     System.out.println("Mohan");
+    //     second();
+    // }
+
+    // public static void second(){
+
+    //     System.out.println("Basit");
+    // }
+
+
+    public static void main(String[] args) {
+
+        // Call
+
+        // first();
+        
+    }
+    
+}
