8d1eebf948eddf72977b5f324f959fd732b37b97
basitdurrani <basitdurrani05786@gmail.com>
2025-05-08 20:43:24 +0530

    Updated

---
+++ Object Oriented Programming/fourth_oops.java
@@ -0,0 +1,21 @@
+// Private Constructor
+
+
+
+public class fourth_oops {
+
+    fourth_oops(){
+
+        System.out.println("I am Private Constructor");
+    }
+
+
+    public static void main(String[] args) {
+
+        fourth_oops sc = new fourth_oops();
+
+      
+        
+    }
+    
+}
--- Object Oriented Programming/third_oops.java
+++ Object Oriented Programming/third_oops.java
@@ -10,7 +10,95 @@
 
 public class third_oops {
 
+
+    // Constructor
+
+    // third_oops(){
+
+    //     System.out.println("Hello I am a Constructor");
+    // }
+
+
+    // int a;
+    // int b;
+
+
+    // third_oops(){
+
+    //     a=100;
+    //     b=200;
+
+    //     System.out.println("First Number Is : "+a);
+    //     System.out.println("Second Number Is : "+b);
+    // }
+
+
+    // Type : 
+
+
+    // Parametermized
+
+
+    // third_oops(String name){
+
+    //     System.out.println("Your Name Is : "+name);
+
+
+    // }
+
+
+    // Copy Constructor
+
+    // int a;
+    // int b;
+
+    // third_oops(){    // Tanishq
+
+    //     a=10;
+    //     b=20;
+
+    //     System.out.println(" Tanishq Copy : A and B value Is : "+a+" "+b);
+
+
+    // }
+
+
+    // third_oops(third_oops paper){   // Jeevesh
+
+    //     a = paper.a;
+    //     b = paper.b;
+
+    //     System.out.println("Jeevesh Copy : A and B value Is : "+a+" "+b);
+        
+    // }
+
+
+
+    
+
     public static void main(String[] args) {
+
+        // third_oops obj = new third_oops();
+
+
+
+        // Parametermized  Call
+
+        // third_oops obj = new third_oops("Basit");
+
+
+        // Copy Constructor
+
+        // third_oops obj = new third_oops();
+
+        // third_oops obj2 = new third_oops(obj);
+
+
+
+
+
+
+       
         
     }
     
